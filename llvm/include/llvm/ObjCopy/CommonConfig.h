0c78c343694b09f9663abd65c8504f969e662a66
Kshitij Paranjape <kshitijvparanjape@gmail.com>
2025-02-08 23:59:51 +0530

    Update CommonConfig.h

--- llvm/include/llvm/ObjCopy/CommonConfig.h
+++ llvm/include/llvm/ObjCopy/CommonConfig.h
@@ -274,7 +274,7 @@ struct CommonConfig {
   bool StripNonAlloc = false;
   bool StripSections = false;
   bool StripUnneeded = false;
-  bool isVerboseEnabled = false;
+  bool Verbose = false;
   bool Weaken = false;
   bool DecompressDebugSections = false;
 
